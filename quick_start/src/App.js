aba40d74aa9073bcb2bc07042c042720bb7c3c37
sousuke-code <msousuke1@gmail.com>
2024-10-21 13:42:25 +0900

    チュートリアルアプリの作成

--- quick_start/src/App.js
+++ quick_start/src/App.js
@@ -8,18 +8,35 @@ function Square({value, onSquareClick}) {
   >{value}</button>
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+ function Board({ xIsNext, squares, onPlay}) {
+  
 
   function handleClick(i) {
+    if (squares[i]  || calculateWinner(squares)) {
+      return ;
+    }
     const nextSuares = squares.slice();
-    nextSuares[i] = "X";
-    setSquares(nextSuares);
+    if (xIsNext) {
+      nextSuares[i] = "X";
+
+    } else {
+     nextSuares[i] = "O";
+    }
+    onPlay(nextSuares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) {
+    status = 'Winner : ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
 
   return (
     <>
+     <div className="status">{status}</div>
      <div className="board-row">
       <Square value={squares[0]} onSquareClick={()=> handleClick(0)}/>
       <Square value={squares[1]} onSquareClick={()=> handleClick(1)}/>
@@ -38,5 +55,69 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
      </div>
     </>
+  )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSuares) {
+    const nextHistory = [...history.slice(0, currentMove +1), nextSuares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length -1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares,move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares ={currentSquares} onPlay ={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   )
 }
